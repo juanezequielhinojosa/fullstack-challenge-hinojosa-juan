ee506e2ecb0cc16a4baf1afafa80cfa8b6036fd4
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2022-09-20 23:17:31 -0300

    corrigiendo estilos

--- mod3-challenge01/src/componentes/Personaje.jsx
+++ mod3-challenge01/src/componentes/Personaje.jsx
@@ -7,7 +7,7 @@ const Personaje=({id,nombre,imagen,estado,especie,setImagenPersonaje})=>{
             
             <img src={imagen} alt={`imagend de ${nombre}`} className="contenedor-imagen"/>
            <div className="contenedor-datos">
-             <p><strong>{nombre}</strong></p>
+            <p><strong>{nombre}</strong></p>
             <p><strong>{estado}</strong></p>
             <p><strong>{especie}</strong></p>
            </div>
